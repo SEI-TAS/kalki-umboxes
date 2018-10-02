c9f36afa01aec4c120872894486b433175a91457
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-02 14:46:03 -0400

    Simple test client.

---
+++ credential-sniffer/test_client.py
@@ -0,0 +1,11 @@
+import httplib
+from base64 import b64encode
+
+connection = httplib.HTTPConnection('localhost', 9010, timeout=10)
+userAndPass = b64encode(b"Username:Password").decode("ascii")
+headers = {'Authorization': 'Basic %s' % userAndPass}
+connection.request("GET", "/foo", headers=headers)
+response = connection.getresponse()
+print("Status: {} and reason: {}".format(response.status, response.reason))
+
+connection.close()
