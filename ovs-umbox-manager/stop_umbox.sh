b0d5a8d9bcdc93bf5877900d9cd2fcd83a9ce23a
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-04-30 14:05:52 -0400

    Added commands to delete rules when stopping a VM

--- ovs-umbox-manager/stop_umbox.sh
+++ ovs-umbox-manager/stop_umbox.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-python umbox.py -c stop -s 192.168.58.102 -u $1
+
+DATA_NODE_IP=192.168.58.102
+
+IOT_DEVICE_IP=192.168.56.103
+
+UMBOX_NAME=$1
+
+python umbox.py -c stop -s $DATA_NODE_IP -u $UMBOX_NAME
+
+bash clear_redirection.sh $DATA_NODE_IP $IOT_DEVICE_IP
