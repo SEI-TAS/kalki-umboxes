9dfdb468b9319bed6c7a056748ca32737a2f27ad
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-06-17 16:23:37 -0400

    moved alertsender and sniffer from old repo

--- run_container.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -p 6060:6060 --rm --net=kalki_nw --name kalki-dni kalki/kalki-dni
--- vm-umbox-tool/ovs-tool/clear_redirection.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# $1: IP address of data node.
-# $2: IP address of device to redirect traffic from.
-python ovs.py -c del_rule -s $1 -di $2
