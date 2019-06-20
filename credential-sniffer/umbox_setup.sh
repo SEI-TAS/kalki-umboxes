844f777d066e48312120fcf6cb93b0672c8d2c73
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-06-20 12:04:12 -0400

    Fixing issues with envs and users

--- credential-sniffer/umbox_setup.sh
+++ credential-sniffer/umbox_setup.sh
@@ -11,10 +11,19 @@ apt-get update
 apt-get install -yqq python-pip
 pip install pipenv
 
+# Make virtualenvs appear in current directory.
+export PIPENV_VENV_IN_PROJECT="enabled"
+
+# Setup Alerter
 cd /home/vagrant/alerter
 pipenv install
 cp alerter.service /etc/systemd/system/
+systemctl enable alerter
+systemctl start alerter
 
+# Setup Credential-Sniffer
 cd /home/vagrant/credential-sniffer
 pipenv install
 cp sniffer.service /etc/systemd/system/
+systemctl enable sniffer
+systemctl start sniffer
