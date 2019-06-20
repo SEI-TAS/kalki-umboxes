4e7b42ed67ff7d9bb422c19b156c70f7ca1e13b5
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-06-20 10:54:17 -0400

    Fixed issue copying files, added proxy setup code

--- credential-sniffer/umbox_setup.sh
+++ credential-sniffer/umbox_setup.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+
+# NOTE: this is only needed if behind a proxy.
+PROXY="http://proxy.sei.cmu.edu:8080"
+echo "Acquire::http::Proxy ${PROXY};" | sudo tee /etc/apt/apt.conf
+export http_proxy=${PROXY}
+export https_proxy=${PROXY}
+
 apt-get update
-apt-get install -yqq python python-pip
+#apt-get install -yqq python
+apt-get install -yqq python-pip
 pip install pipenv
 
-cd /home/umbox/alerter
+cd /home/vagrant/alerter
 pipenv install
 cp alerter.service /etc/systemd/system/
 
-cd /home/umbox/credential-sniffer
+cd /home/vagrant/credential-sniffer
 pipenv install
 cp sniffer.service /etc/systemd/system/
