4ac14011b0e97c1dd06988132440d4186c47b765
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-06-21 10:54:15 -0400

    Moved DNS server setup to proxy bash file

--- baseline/proxy_setup.sh
+++ baseline/proxy_setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Setup DNS resolution. This is needed to avoid issues with proxies when resolving names.
+# This IP is the gateway/DNS of the default libvirt network we are using.
+echo "nameserver 192.168.121.1" > /etc/resolv.conf
+
 # NOTE: this is only needed if behind a proxy.
 PROXY="http://proxy.sei.cmu.edu:8080"
 echo "Acquire::http::Proxy \"${PROXY}\";" | sudo tee /etc/apt/apt.conf
