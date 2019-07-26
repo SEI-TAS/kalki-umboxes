10638b7715a4aef0703b7116fbc8394f73c64e4c
connormullaly <connor.f.mullaly@gmail.com>
2019-07-26 14:50:33 -0400

    adding automated phillips hue brute force test script

--- sniffer/testScripts/phillips_random_token.sh
+++ sniffer/testScripts/phillips_random_token.sh
@@ -4,4 +4,4 @@ export http_proxy='';
 # bash generate random 16 character alphanumeric string (upper and lowercase) 
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
-curl -s -o /dev/null -w "Status: %{http_code}\n" http://localhost:9010/api/"$NEW_UUID"/lights
+curl -s -o /dev/null http://localhost:9010/api/"$NEW_UUID"/lights
