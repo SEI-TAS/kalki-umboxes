692659160511bc95199e5465dd68d226f4315476
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-08-23 10:11:13 -0400

    Added iface config in /etc/network/interfaces

--- umboxes/baseline/umbox_setup.sh
+++ umboxes/baseline/umbox_setup.sh
@@ -2,3 +2,10 @@
 
 # Decrease DHCP timeout.
 sed -i 's/timeout 300/timeout 15/g' /etc/dhcp/dhclient.conf
+
+# Setup network interfaces.
+echo -e "auto lo\niface lo inet loopback"  > /etc/network/interfaces
+echo -e "auto eth0\niface eth0 inet dhcp" >> /etc/network/interfaces
+echo -e "auto eth1\niface eth1 inet dhcp" >> /etc/network/interfaces
+echo -e "auto eth2\niface eth2 inet dhcp" >> /etc/network/interfaces
+echo -e "auto eth3\niface eth3 inet dhcp" >> /etc/network/interfaces
