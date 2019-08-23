15124968e2070657b9c43021355055852dc670da
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-08-23 16:31:36 -0400

    Fixed automated apt

--- umboxes/sniffer_base/sniffer_setup.sh
+++ umboxes/sniffer_base/sniffer_setup.sh
@@ -5,7 +5,7 @@ SNIFFER_HOME=$1
 # Install Python 3.6.
 sudo -E add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.6
+sudo apt-get install -yqq python3.6
 
 # Setup Sniffer
 cd ${SNIFFER_HOME}
