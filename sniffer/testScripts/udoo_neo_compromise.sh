2ba275bfec54c3d1915df77df8c367af7317e611
Marc Novakouski <novakom@sei.cmu.edu>
2019-09-30 17:18:01 -0400

    bug fixes for compromise script 3

--- sniffer/testScripts/udoo_neo_compromise.sh
+++ sniffer/testScripts/udoo_neo_compromise.sh
@@ -30,33 +30,33 @@ tcpPackets=0
 tcpPort=31
 
 # Read Parameters
-while ["$1" != ""]; do
+while [ "$1" != "" ]; do
     case "$1" in
         -h ) usage
               exit ;;
-        -s ) $sourceIP = $1 ;;
-        -d ) $destIP = $1 ;;
-        -u ) $udpPackets = $1 ;;
-        -t ) $tcpPackets = $1 ;;
-        -p ) $tcpPort = $1 ;;
+        -s ) $sourceIP=$1 ;;
+        -d ) $destIP=$1 ;;
+        -u ) $udpPackets=$1 ;;
+        -t ) $tcpPackets=$1 ;;
+        -p ) $tcpPort=$1 ;;
     esac
     shift
 done
 
 # Check sourceIP validity; must not be empty
-if [$sourceIP = ""]; then
+if [ $sourceIP = "" ]; then
     echo "Invalid sourceIP; exiting"
     exit
 fi
 
 # Check packet count validity; at least one must be greater than zero
-if [$udpPackets -lt 1 && $tcpPackets -lt 1]; then
+if [ $udpPackets -lt 1 && $tcpPackets -lt 1 ]; then
     echo "Zero packets to be sent; exiting"
     exit
 fi
 
 # Check port validity; 22 is allowed for ssh traffic (though will not result in compromise), but must be 0-65535
-if [$tcpPort -lt 0 || $tcpPort -gt 65535]; then
+if [ $tcpPort -lt 0 || $tcpPort -gt 65535 ]; then
     echo "TCP Port out of range; exiting"
     exit
 fi
@@ -64,24 +64,24 @@ fi
 echo "Executing script with source IP $sourceIP, dest IP $destIP, UDP Packets $udpPackets, TCP Packets $tcpPackets, TCP Port $tcpPort"
 
 # Check the UDP packet count
-if [$udpPackets -gt 0]; then
+if [ $udpPackets -gt 0 ]; then
     # Perform UDP Packet sends for as many as configured
     for i in {1..$udpPackets}
     do
         # Vary destination port to ensure that it does not trigger the duplicate packet check
-        $destPort = 20 + (i * 5)
+        $destPort=i*5+20
         sudo nping --udp --source-ip $sourceIP $destIP -p $destPort -c 1
         sleep 1
     done
 fi
 
 # Check the TCP packet count
-if [$tcpPackets -gt 0]; then
+if [ $tcpPackets -gt 0 ]; then
     # Perform TCP Packet sends for as many as configured
     for i in {1..$tcpPackets}
     do
         # Vary destination port to ensure that it does not trigger the duplicate packet check
-        $destPort = 30 + (i * 5)
+        $destPort=i*5+30
         sudo nping --tcp --source-ip $sourceIP $destIP -p $destPort -g $tcpPort -c 1
         sleep 1
     done
