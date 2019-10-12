b8de9e0cc5933560281fd6713eb8a2a8f08e5d69
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-11 23:07:47 -0400

    Avoid deletion prompt

--- umboxes/deploy.sh
+++ umboxes/deploy.sh
@@ -4,4 +4,4 @@ UMBOX_FOLDER=${PWD##*/}
 
 cp *_standalone.qcow2 ../../../kalki-umbox-controller/clone-image-server/images/${UMBOX_FOLDER}.qcow2
 echo "Copied, deleting original"
-rm *_standalone.qcow2
+rm -f *_standalone.qcow2
