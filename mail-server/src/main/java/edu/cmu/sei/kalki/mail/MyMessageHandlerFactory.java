7403300332acfb9cb7e8d603d2e3a4f441a66b8e
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-12 23:14:34 -0400

    Adding simple mail server

---
+++ mail-server/src/main/java/edu/cmu/sei/kalki/mail/MyMessageHandlerFactory.java
@@ -0,0 +1,67 @@
+package edu.cmu.sei.kalki.mail;
+
+import org.subethamail.smtp.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MyMessageHandlerFactory implements MessageHandlerFactory {
+
+    MailServer mailServer;
+
+    public MyMessageHandlerFactory(MailServer mailServer){
+        this.mailServer = mailServer;
+    }
+
+    public MessageHandler create(MessageContext ctx) {
+        return new Handler(ctx);
+    }
+
+    class Handler implements MessageHandler {
+        MessageContext ctx;
+
+        public Handler(MessageContext ctx) {
+            this.ctx = ctx;
+        }
+
+        public void from(String from) throws RejectException {
+//            System.out.println("FROM:"+from);
+            MailServer.notify(from);
+        }
+
+        public void recipient(String recipient) throws RejectException {
+//            System.out.println("RECIPIENT:"+recipient);
+        }
+
+        public void data(InputStream data) throws IOException
+        {
+//            System.out.println("MAIL DATA");
+//            System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+//            System.out.println(this.convertStreamToString(data));
+//            System.out.println("Image goes here");
+//            System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        }
+
+        public void done() {
+//            System.out.println("Finished");
+        }
+
+        public String convertStreamToString(InputStream is) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+
+    }
+}
---
+++ mail-server/src/main/java/edu/cmu/sei/kalki/mail/SendMail.java
@@ -0,0 +1,76 @@
+package edu.cmu.sei.kalki.mail;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class SendMail implements EventObserver {
+
+    public static void main(String[] args)
+    {
+        MailServer.initialize();
+        MailServer.registerObserver(new SendMail());
+        //sendMail();
+
+        System.out.println("Waiting for future emails.");
+        while(true) {
+            try
+            {
+                Thread.sleep(1000);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void sendMail() {
+        // Recipient's email ID needs to be mentioned.
+        String to = "test@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "camera1@dlink.com";
+
+        // Assuming you are sending email from localhost
+        String host = "localhost";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.smtp.port", String.valueOf(MailServer.MAIL_PORT));
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is actual message");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void notify(String message)
+    {
+        System.out.println("Message received!: " + message);
+    }
+}
