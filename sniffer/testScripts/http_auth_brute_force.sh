bbb4fc5dafcab302b9f081e65af373fae03bcbdb
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-13 11:13:39 -0400

    Updated brute force scripts to actually do that

--- sniffer/testScripts/http_auth_brute_force.sh
+++ sniffer/testScripts/http_auth_brute_force.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+
+CAMERA=10.27.151.117
+PROXYPORT=9010
 export http_proxy='';
-HOST=localhost
-watch -n 1 curl -s -o /dev/null -w "Status: %{http_code}\n" -u Username:Password http://${HOST}:9010/test_server.sh
+
+# Try several times
+for i in {1..15}
+do
+    # bash generate random 16 character alphanumeric string (upper and lowercase)
+    NEW_PASS="pass${i}"
+
+    curl -s -o /dev/null -w "Status: %{http_code}\n" -u user1:${NEW_PASS} http://${CAMERA}:${PROXYPORT}/
+    sleep 0.5
+done
