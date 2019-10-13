bbbd3a1f40755207af1be2bf904cbd804e63cb7d
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-13 10:49:06 -0400

    Clarity

--- sniffer/testScripts/http_proxy_test.sh
+++ sniffer/testScripts/http_proxy_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SERVER=10.27.151.117
+CAMERA=10.27.151.117
 CAMERAPORT=80
 PROXYPORT=9010
 USER=kuser
@@ -8,13 +8,13 @@ PASS=kpass
 export http_proxy='';
 
 # Not authenticated.
-curl -s -o /dev/null -w "Status: %{http_code}\n" http://${SERVER}:${CAMERAPORT}/Image.jpg
+curl -s -o /dev/null -w "Status: %{http_code}\n" http://${CAMERA}:${CAMERAPORT}/Image.jpg
 
 # Wrong credentials.
-curl -s -o /dev/null -w "Status: %{http_code}\n" -u Wrong:Wrong http://${SERVER}:${PROXYPORT}/
+curl -s -o /dev/null -w "Status: %{http_code}\n" -u Wrong:Wrong http://${CAMERA}:${PROXYPORT}/
 
 # Appropriate credentials.
-curl -s -o /dev/null -w "Status: %{http_code}\n" -u ${USER}:${PASS} http://${SERVER}:${PROXYPORT}/
+curl -s -o /dev/null -w "Status: %{http_code}\n" -u ${USER}:${PASS} http://${CAMERA}:${PROXYPORT}/
 
 # API call.
-curl -s -o /dev/null -w "Status: %{http_code}\n" http://${SERVER}:${CAMERAPORT}/Image.jpg
+curl -s -o /dev/null -w "Status: %{http_code}\n" http://${CAMERA}:${CAMERAPORT}/Image.jpg
