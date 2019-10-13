e6ebb5bffcd9757a372f68b113e470e91cac6757
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-13 11:00:39 -0400

    Fixed to send remotely

--- sniffer/testScripts/compromised.py
+++ sniffer/testScripts/compromised.py
@@ -1,8 +1,8 @@
 import socket
 import time
 
-IP = "127.0.0.1"
-PORT = 5005
+IP = "10.27.152.5"
+PORT = 6000
 MESSAGE = "test"
 PACKETS = 10
 
@@ -11,7 +11,7 @@ udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 for i in range(PACKETS):
     curr_port = PORT+i
-    print("Sending UDP packet")
+    print("Sending UDP packet to port " + curr_port)
     udp_socket.sendto(MESSAGE, (IP, curr_port))
     time.sleep(1)
 
