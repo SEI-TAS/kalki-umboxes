ed1cb5a6e7104bf4559d8daaa55e288b7507242b
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-14 15:39:12 -0400

    Updating phillips brute force script

--- sniffer/testScripts/phillips_brute_force.sh
+++ sniffer/testScripts/phillips_brute_force.sh
@@ -3,6 +3,9 @@
 PHILLIPSIP=10.27.151.106
 PHILLIPSPORT=80
 
+export HTTP_PROXY=''
+export http_proxy=''
+
 #20 identical API calls that should not trigger a brute force
 #test_token="abcdefghijklmnop"
 #for i in {1..20}
@@ -30,7 +33,7 @@ do
     # bash generate random 16 character alphanumeric string (upper and lowercase) 
     NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
-    curl -s -o /dev/null http://${PHILLIPSIP}:${PHILLIPSPORT}/api/"$NEW_UUID"/lights
+    wget http://${PHILLIPSIP}:${PHILLIPSPORT}/api/"$NEW_UUID"/lights
     sleep 0.5
 done
 
