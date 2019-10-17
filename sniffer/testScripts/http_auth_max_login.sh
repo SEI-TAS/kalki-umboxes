eac5bcfa2d453ee976be235a9abb3f2b5171ac26
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-17 16:52:28 -0400

    Fixed port

--- sniffer/testScripts/http_auth_max_login.sh
+++ sniffer/testScripts/http_auth_max_login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CAMERA=10.27.151.114  # WiFi AP IP since camera won't return to another private LAN.
-CAMERAPORT=8080
+CAMERA=10.27.151.114 
+CAMERAPORT=80
 PROXYPORT=9010
 export http_proxy='';
 
