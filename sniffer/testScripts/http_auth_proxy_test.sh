b5b939d3ed16dfb81c6d36fa2bc6fa0855b4df15
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-11-08 12:26:42 -0500

    Restructured test script for proxy

--- sniffer/testScripts/http_auth_proxy_test.sh
+++ sniffer/testScripts/http_auth_proxy_test.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-CAMERA=10.27.151.117
-CAMERAPORT=8080
-PROXYPORT=9010
+DEVICE_IP=10.27.151.114
+DEVICE_PORT=80
+PROXY_PORT=9010
 USER=kuser
 PASS=kpass
 export http_proxy='';
 
-# Not authenticated.
-curl -s -o /dev/null -w "Status: %{http_code}\n" http://${CAMERA}:${CAMERAPORT}/
+OPTION=$1
 
-# Wrong credentials.
-curl -s -o /dev/null -w "Status: %{http_code}\n" -u Wrong:Wrong http://${CAMERA}:${PROXYPORT}/
-
-# Appropriate credentials.
-curl -s -o /dev/null -w "Status: %{http_code}\n" -u ${USER}:${PASS} http://${CAMERA}:${PROXYPORT}/
-
-# API call.
-curl -s -o /dev/null -w "Status: %{http_code}\n" http://${CAMERA}:${CAMERAPORT}/
+if ["$OPTION" == "API"]; then
+    # API call.
+    curl -s -o /dev/null -w "Status: %{http_code}\n" http://${DEVICE_IP}:${DEVICE_PORT}/
+elif ["$OPTION" == "WRONG"]; then
+    # Wrong credentials.
+    curl -s -o /dev/null -w "Status: %{http_code}\n" -u Wrong:Wrong http://${DEVICE_IP}:${PROXY_PORT}/
+elif ["$OPTION" == "RIGHT"]; then
+    # Appropriate credentials.
+    curl -s -o /dev/null -w "Status: %{http_code}\n" -u ${USER}:${PASS} http://${DEVICE_IP}:${PROXY_PORT}/
+fi
