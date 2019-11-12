1558208872c65be1d55d3ff7acbfdf4477da500b
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-11-12 15:19:55 -0500

    Moved stat and email functionalities to separate modules

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -6,11 +6,6 @@ import logging
 import sys
 import json
 import traceback
-import time
-
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 import netifaces
 
@@ -22,6 +17,9 @@ from packetHandlers.httpAuthHandler import HttpAuthHandler
 from packetHandlers.phillipsHueHandler import PhillipsHueHandler
 from packetHandlers.ipConnectionsHandler import IpConnectionsHandler
 
+from utils import stats
+from utils import email
+
 # Internal parameters.
 LOG_FILE_PATH = "sniffer.log"
 
@@ -31,14 +29,6 @@ ETH_P_ALL = 3
 # Global logger.
 logger = None
 
-# Global handler.
-handler = None
-
-# Global stats map
-packet_stats_total = None
-packet_stats_senders = None
-packet_stats_targets = None
-packet_stats_last_log_time = None
 
 class HandlerResults:
 
@@ -46,14 +36,6 @@ class HandlerResults:
         self.echo_decision = True
         self.issues_found = []
         self.direct_messages_to_send = []
-
-class PacketStats:
-
-    def __init__(self):
-        self.packet_count = 0
-        self.tcp_count = 0
-        self.udp_count = 0
-        self.other_count  = 0
 
 
 def setup_custom_logger(name, file_path):
@@ -69,125 +51,13 @@ def setup_custom_logger(name, file_path):
     logger.addHandler(screen_handler)
     return logger
 
+
 def load_config():
     """Loads config from external file."""
     with open("config.json") as json_config:
         config = json.load(json_config)
     return config
 
-def setup_email_login(host, port, username, password):
-    # Try to connect 3 times
-    retry_count = 3
-    while retry_count > 0:
-        try:
-            # Establish Connection
-            email_server = smtplib.SMTP(host=host, port=port, timeout=30)
-
-            # Start TLS
-            email_server.starttls()
-            email_server.login(username, password)
-
-            # Successful connection; return the server object
-            print ("Email reporting set up successfully.", flush=True)
-            return email_server
-        except: #socket.timeout:
-            print("Failed to connect to " + host + ":" + port + "; Exception: " + sys.exc_info()[0] + "; trying again", flush=True)
-            retry_count = retry_count - 1
-
-    print ("Failed to connect to " + host + ":" + port + ".  Email unavailable.", flush=True)
-    return None
-
-def setup_email_no_login(host, port):
-    # Try to connect 3 times
-    retry_count = 3
-    while retry_count > 0:
-        try:
-            # Establish Connection
-            email_server = smtplib.SMTP(host=host, port=port, timeout=30)
-
-            # Successful connection; return the server object
-            print ("Email reporting set up successfully.", flush=True)
-            return email_server
-        except socket.timeout:
-            print("Failed to connect to " + host + ":" + port + ", trying again", flush=True)
-            retry_count = retry_count - 1
-
-    print ("Failed to connect to " + host + ":" + port + ".  Email unavailable.", flush=True)
-    return None
-
-def send_email(email_server, source_address, destination_address, email_subject, email_body):
-    # Make sure the email server is connected
-    if email_server == None:
-        print ("No valid email server connection.  Email not sent.", flush=True)
-        return
-
-        # Email server connection confirmed; Create the message to send
-    msg = MIMEMultipart()
-    msg['From'] = source_address
-    msg['To'] = destination_address
-    msg['Subject'] = email_subject
-    msg.attach(MIMEText(email_body, 'plain'))
-
-    # Send the message via the provided server
-    try:
-        email_server.send_message(msg)
-    except smtplib.SMTPException:
-        print("Failed to send email to " + destination_address, flush=True)
-
-    # Clean up the message
-    del msg
-
-
-def logPacketStatistics(ip_packet, log_interval, logger):
-    # Log the current packet to the total stats
-    packet_stats_total.packet_count += 1
-
-    if ip_packet.proto == 6:
-        packet_stats_total.tcp_count += 1
-    elif ip_packet.proto == 17:
-        packet_stats_total.udp_count += 1
-    else:
-        packet_stats_total.other_count += 1
-
-    # Now log based on Senders
-    if ip_packet.src not in packet_stats_senders:
-        new_sender = PacketStats()
-        packet_stats_senders[ip_packet.src] = new_sender
-
-    packet_stats_senders[ip_packet.src].packet_count += 1
-    if ip_packet.proto == 6:
-        packet_stats_senders[ip_packet.src].tcp_count += 1
-    elif ip_packet.proto == 17:
-        packet_stats_senders[ip_packet.src].udp_count += 1
-    else:
-        packet_stats_senders[ip_packet.src].other_count += 1
-
-    # Now log based on Receivers
-    if ip_packet.target not in packet_stats_targets:
-        new_target = PacketStats()
-        packet_stats_targets[ip_packet.target] = new_target
-
-    packet_stats_targets[ip_packet.target].packet_count += 1
-    if ip_packet.proto == 6:
-        packet_stats_targets[ip_packet.target].tcp_count += 1
-    elif ip_packet.proto == 17:
-        packet_stats_targets[ip_packet.target].udp_count += 1
-    else:
-        packet_stats_targets[ip_packet.target].other_count += 1
-
-    # Now log based on timer
-    global packet_stats_last_log_time
-    if time.time() - packet_stats_last_log_time > log_interval:
-        msg = "LOG_INFO: Packet stats total: {} tcp: {} udp: {} other: {}".format(packet_stats_total.packet_count, packet_stats_total.tcp_count, packet_stats_total.udp_count, packet_stats_total.other_count)
-        logger.warning(msg)
-        for sender_ip in packet_stats_senders:
-            msg = "LOG_INFO: Packets sent from {} stats total: {} tcp: {} udp: {} other: {}".format(sender_ip, packet_stats_total.packet_count, packet_stats_total.tcp_count, packet_stats_total.udp_count, packet_stats_total.other_count)
-            logger.warning(msg)
-        for target_ip in packet_stats_targets:
-            msg = "LOG_INFO: Packets sent to {} stats total: {} tcp: {} udp: {} other: {}".format(target_ip, packet_stats_total.packet_count, packet_stats_total.tcp_count, packet_stats_total.udp_count, packet_stats_total.other_count)
-            logger.warning(msg)
-
-        packet_stats_last_log_time = time.time()
 
 def main():
     global logger
@@ -195,46 +65,27 @@ def main():
 
     config = load_config()
 
-    global packet_stats_total
-    global packet_stats_last_log_time
-    global packet_stats_senders
-    global packet_stats_targets
-    if config["stat_logging"] == "on":
-        packet_stats_total = PacketStats()
-        packet_stats_last_log_time = time.time()
-        packet_stats_senders = {}
-        packet_stats_targets = {}
-
     handler_names = config["handlers"]
     echo_on = config["echo"] == "on"
-    email_on = config["email"] == "on"
     restricted_list = config["restrictedIPs"]
 
-    # Set up email if enabled in the configuration
-    if email_on == True:
-        # Get the basic email attributes
-        email_server_address = config["emailConfig"]["email_server_address"]
-        email_server_port = config["emailConfig"]["email_server_port"]
-        email_source_address = config["emailConfig"]["email_source_address"]
+    # Set up stat logging if enabled in the configuration
+    stat_logging_on = config["stat_logging"] == "on"
+    if stat_logging_on:
+        stat_logger = stats.StatLogger(logger, config["stat_interval"])
 
-        # Check to see if the server needs to be logged into
-        if config["emailConfig"]["email_server_login"] == "on":
-            # Get the username/password for logging in, and execute log in process
-            email_username = config["emailConfig"]["email_account_username"]
-            email_password = config["emailConfig"]["email_account_password"]
-            email_server = setup_email_login(email_server_address, email_server_port, email_username, email_password)
-        else:
-            # Set up email server without logging in
-            email_server = setup_email_no_login(email_server_address, email_server_port)
+    # Set up email if enabled in the configuration
+    email_on = config["email"] == "on"
+    if email_on is True:
+        email_server = email.EmailServer(config["emailConfig"])
 
     # Set up combined result object
     combined_results = HandlerResults()
 
-    #use the passed in command line arguments to create and set the correct handler
-    #global handlers
+    # Use the passed in command line arguments to create and set the correct handler
     handlers = []
     for handler_name in handler_names:
-        if handler_name ==  "httpAuth":
+        if handler_name == "httpAuth":
             handlers.append(HttpAuthHandler(config, logger, combined_results))
         elif handler_name == "phillipsHue":
             handlers.append(PhillipsHueHandler(config, logger, combined_results))
@@ -243,9 +94,9 @@ def main():
         else:
             print("Invalid handler name {} in config file".format(handler_name), flush=True)
     if len(handlers) == 0:
-        print ('No valid handler names found, proceeding with zero configured handlers.', flush=True)
+        print('No valid handler names found, proceeding with zero configured handlers.', flush=True)
     else:
-        print ("Initializing handlers {}".format(handlers), flush=True)
+        print("Initializing handlers {}".format(handlers), flush=True)
 
     incoming = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
     incoming.bind((config["incomingNIC"], 0))
@@ -267,7 +118,6 @@ def main():
 
     direct_nic_mac = netifaces.ifaddresses(config["directNIC"])[netifaces.AF_LINK][0]['addr']
     print("Local MAC on direct NIC is {}\n".format(direct_nic_mac), flush=True)
-
 
     last_data = None
     last_echo = None
@@ -326,9 +176,9 @@ def main():
                 for action in config["action_list"][result]:
                     if action == "ALERT":
                         print("ALERT: " + result + " detected!", flush=True)
-                    elif action == "EMAIL" and email_on == True:
+                    elif action == "EMAIL" and email_on:
                         for destination_address in config["emailConfig"]["email_destination_address_list"]:
-                            send_email(email_server, email_source_address, destination_address, 'Alert: ' + result, result + ' attempt detected from ' + ipv4.src)
+                            email_server.send_email(destination_address, 'Alert: ' + result, result + ' attempt detected from ' + ipv4.src)
                     elif action == "BLACKLIST":
                         print(result + " attempt detected from " + ipv4.src + "; adding to restricted list", flush=True)
                         restricted_list.append(ipv4.src)
@@ -339,8 +189,8 @@ def main():
                 direct.send(message)
 
             # Do stat logging if enabled
-            if config["stat_logging"] == "on":
-                logPacketStatistics(ipv4, config["stat_interval"], logger)
+            if stat_logging_on:
+                stat_logger.log_packet_statistics(ipv4)
 
         # Only echo packet if echo is on and src IP is not restricted
         if echo_on and (ipv4 is not None and ipv4.src not in restricted_list) and combined_results.echo_decision and last_echo != raw_data:
@@ -349,6 +199,7 @@ def main():
                 last_echo = raw_data
             except Exception as e:
                 print("Error echoing: " + str(e))
+
 
 if __name__ == '__main__':
     main()
