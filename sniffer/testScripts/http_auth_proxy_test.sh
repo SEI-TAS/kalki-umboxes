ed9448878e93b29aaf56a4fe44c49ab864e3be87
Sebastian Echeverria <secheverria@sei.cmu.edu>
2019-11-21 11:48:31 -0500

    Improving auth proxy script tester

--- sniffer/testScripts/http_auth_proxy_test.sh
+++ sniffer/testScripts/http_auth_proxy_test.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-DEVICE_IP=10.27.151.114
-DEVICE_PORT=80
+# Information configred in the auth proxy.
 PROXY_PORT=9010
-USER=kuser
-PASS=kpass
+RIGHT_USER=kuser
+RIGHT_PASS=kpass
+
+# Action and IoT device info, get from command line or use defaults.
+OPTION=${1:-API}
+DEVICE_IP=${2:-'10.27.151.114'}
+DEVICE_PORT=${3:-80}
+
+# Disable proxy, if any.
 export http_proxy='';
 
-OPTION=$1
-
-# Test code to verify command line processing
-
+# Command line argument tells us what to do.
 if [ "$OPTION" = "API" ]; then
     # API call.
     curl -s -o /dev/null -w "Status: %{http_code}\n" http://${DEVICE_IP}:${DEVICE_PORT}/
@@ -19,7 +22,5 @@ elif [ "$OPTION" = "WRONG" ]; then
     curl -s -o /dev/null -w "Status: %{http_code}\n" -u Wrong:Wrong http://${DEVICE_IP}:${PROXY_PORT}/
 elif [ "$OPTION" = "RIGHT" ]; then
     # Appropriate credentials.
-    curl -s -o /dev/null -w "Status: %{http_code}\n" -u ${USER}:${PASS} http://${DEVICE_IP}:${PROXY_PORT}/
+    curl -s -o /dev/null -w "Status: %{http_code}\n" -u ${RIGHT_USER}:${RIGHT_PASS} http://${DEVICE_IP}:${PROXY_PORT}/
 fi
-
-
