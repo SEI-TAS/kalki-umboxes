feec9e8caa7f403848a48a5293082219bcf963e1
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-10 12:18:32 -0500

    Changes to access folders outside the umbox one

--- umboxes/u5-sniffer-log-stats/build_docker.sh
+++ umboxes/u5-sniffer-log-stats/build_docker.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t u5 -f umboxes/u5-sniffer-log-stats/Dockerfile .
+UMBOX_NAME=u5
+PATH_TO_UMBOX=umboxes/u5-sniffer-log-stats
+
+docker build -t ${UMBOX_NAME} --build-arg UMBOX_PATH=${PATH_TO_UMBOX} -f ${PATH_TO_UMBOX}/Dockerfile .
