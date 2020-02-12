03df23d066284d18ec79e93c593bea9ea5a52bd0
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-02-12 14:02:51 -0500

    Updated readme with info from wiki

--- umboxes/vm_deploy.sh
+++ umboxes/vm_deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-UMBOX_FOLDER=${PWD##*/}
-
 # NOTE: assumes kalki umbox controller is installed in this folder. Change if different.
-UMBOX_CONTROLLER_FOLDER=/home/local/SSDLAB/secheverria/kalki/kalki-umbox-controller
-sudo cp *_standalone.qcow2 ${UMBOX_CONTROLLER_FOLDER}/clone-image-server/images/${UMBOX_FOLDER}.qcow2
+UMBOX_IMAGES_FOLDER=/home/local/SSDLAB/secheverria/kalki/kalki-umbox-controller/clone-image-server/images
+
+UMBOX_NAME=${PWD##*/}
+sudo cp *_standalone.qcow2 ${UMBOX_IMAGES_FOLDER}/${UMBOX_NAME}.qcow2
 echo "Copied, deleting original"
 rm -f *_standalone.qcow2
