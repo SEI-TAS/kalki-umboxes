3731eed189547e27810d706dea1c9b6dcc566942
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-02-12 18:00:00 -0500

    Trying to fix weird python setup issue

--- umboxes/common/alerter_vm_base/alerter_setup.sh
+++ umboxes/common/alerter_vm_base/alerter_setup.sh
@@ -6,6 +6,7 @@ ALERTER_HOME=$1
 apt-get update
 apt-get install -yqq python3 python3-pip
 pip3 install pipenv
+pip3 install setuptools --upgrade
 
 # Make virtualenvs appear in current directory.
 echo "export PIPENV_VENV_IN_PROJECT=\"enabled\"" >> $HOME/.profile
