c4213791dafdad6434aba0382982933130a2ade2
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-06-12 16:06:23 -0400

    Escaping char

--- umboxes/deployments/build_umbox_image.sh
+++ umboxes/deployments/build_umbox_image.sh
@@ -3,7 +3,7 @@
 UMBOX_NAME="$1"
 DEPLOYMENT="$2"
 docker build \
-  -t kalki-${DEPLOYMENT}\${UMBOX_NAME} \
+  -t kalki-${DEPLOYMENT}\\${UMBOX_NAME} \
   --build-arg UMBOX_IMAGE=$UMBOX_NAME \
   --build-arg DEPLOYMENT=$DEPLOYMENT \
   .
