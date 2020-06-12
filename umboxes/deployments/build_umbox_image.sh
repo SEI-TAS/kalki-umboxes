0222331304cb81c1824cea74890dcb67d59d3719
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-06-12 15:59:37 -0400

    Actually passing arguments

--- umboxes/deployments/build_umbox_image.sh
+++ umboxes/deployments/build_umbox_image.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-UMBOX_NAME="${1}"
-DEPLOYMENT="${2}"
-docker build -t ${UMBOX_NAME}-${DEPLOYMENT} -ARGS=<TODO> .
+UMBOX_NAME="$1"
+DEPLOYMENT="$2"
+docker build \
+  -t ${UMBOX_NAME}-${DEPLOYMENT} \
+  --build-arg UMBOX_IMAGE=$UMBOX_NAME \
+  --build-arg DEPLOYMENT=$DEPLOYMENT \
+  .
