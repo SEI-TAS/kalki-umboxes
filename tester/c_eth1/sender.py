2e1473baecb6d39e6085662e9ea0e194acf286a4
Jun Kimm <TheJYKoder@gmail.com>
2020-06-15 11:58:17 -0400

    Created baseline code for eth2 and eth3. Merged reset.py and main.py

--- tester/c_eth1/sender.py
+++ tester/c_eth1/sender.py
@@ -1,17 +1,35 @@
 import socket
+import subprocess
+import json
+import binascii
+
+import struct
 
 ETH_P_ALL = 3
+network = "eth1"
+src = "e1"
+dest = "umbox"
+
+def createEthFrameHeader(dest_mac, src_mac):
+    ETH_P_IP = 0x0800
+    rawFrame = struct.pack("!6s6sH", binascii.unhexlify(dest_mac.replace(":","")), binascii.unhexlify(src_mac.replace(":","")),ETH_P_IP)
+    return rawFrame
+
+
 
 raw_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
-raw_socket.bind(("eth1", 0))
+raw_socket.bind((network, 0))
 
-src_addr = "\x01\x02\x03\x04\x05\x06"
-dst_addr = "\x01\x02\x03\x04\x05\x06"
+src_Mac = "02:42:ac:1f:00:03"
+dest_Mac = "02:42:ac:1f:00:02"
+
+destSrcString = createEthFrameHeader(dest_Mac, src_Mac)
+
 payload = ("["*30)+"PAYLOAD"+("]"*30)
-checksum = "\x1a\x2b\x3c\x4d"
 ethertype = "\x08\x01"
 
-data = dst_addr+src_addr+ethertype+payload+checksum
-raw_socket.send(data.encode())
-
+data = destSrcString+ethertype+payload.encode()
 print("success")
+while(True):
+	#print("D")
+	raw_socket.send(data)
--- tester/main.py
+++ tester/main.py
@@ -2,11 +2,16 @@ import subprocess
 
 tool_pipe = subprocess.PIPE
 
+#Reset
+subprocess.Popen("docker stop $(docker ps -a -q)", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker rm $(docker ps -a -q)", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker system prune", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate(input=b"y")
+
 #Create the containers
 subprocess.Popen("docker container create -it --name umbox u0-sniffer-based", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
-subprocess.Popen("docker container create -it --name e1 c_eth1", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
-subprocess.Popen("docker run -d --name e2 library/alpine:3.5 sleep 3600d", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
-subprocess.Popen("docker run -d --name e3 library/alpine:3.5 sleep 3600d", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker container create -it --name e1 --mac-address 00:00:00:00:00:11 c_eth1", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker container create -it --name e2 c_eth2", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker container create -it --name e3 c_eth3", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
 
 #Create the networks
 subprocess.Popen("docker network create eth1", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
@@ -24,3 +29,5 @@ subprocess.Popen("docker network connect eth3 e3", shell=True, stdin=tool_pipe, 
 #Run the containers
 subprocess.Popen("docker container start umbox", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
 subprocess.Popen("docker container start e1", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker container start e2", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
+subprocess.Popen("docker container start e3", shell=True, stdin=tool_pipe, stdout=tool_pipe, stderr=tool_pipe).communicate()
