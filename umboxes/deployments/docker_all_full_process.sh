4e6fbb53fab636b1fb783ad950bebedc24ad8750
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-03 11:26:17 -0400

    Adding script to simplify rebuilding all umboxes for a given deployment

---
+++ umboxes/deployments/docker_all_full_process.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DEPLOYMENT="$2"
+
+update_umbox() {
+    local umbox_name="$1"
+
+    echo "Updating ${umbox_name}"
+    time bash build_umbox_image.sh ${umbox_name} ${DEPLOYMENT}
+    echo "Finished updating ${umbox_name}"
+}
+
+echo "Updating all docker umboxes for deployment ${DEPLOYMENT}"
+
+for DIR in ../standard/*/   # list directories with full path
+do
+    DIR=${DIR%*/}          # remove the trailing "/"
+    UMBOX_NAME=${DIR##*/}  # remove all path before current directory
+    update_umbox ${UMBOX_NAME}
+done
+
+echo "Finished updating all docker umboxes"
